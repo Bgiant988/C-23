bfa66373b13dcfa5cb698562131e710519f4d2ee
Bgiant988 <77132872+Bgiant988@users.noreply.github.com>
2021-01-28 19:43:08 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C-23
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(1000, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  movingRect.velocityX=5
+  fixedRect.velocityX=-5
+}
+
+function draw() {
+  background(0,0,0);  
+  //movingRect.x = World.mouseX;
+  //movingRect.y = World.mouseY;
+
+ bounceOff(movingRect,fixedRect);
+  drawSprites();
+}
+
+
+
+
+
